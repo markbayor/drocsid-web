613d66984fb98d7cbe0fd34646b4c61d8f3e56c5
Mark Baydoun <markosbo95@gmail.com>
2020-05-24 05:21:29 +0200

    most functionalities working

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -1,4 +1,4 @@
-const User = require('../db/models/user')
+const User = require('../db/models/User')
 
 const loginUser = async (req, res, next) => {
   try {
--- server/auth/index.js
+++ server/auth/index.js
@@ -1,5 +1,4 @@
 const router = require('express').Router()
-const User = require('../db/models/user')
 module.exports = router
 
 const { loginUser, signUpUser, logOutUser, getMe } = require('./auth')
