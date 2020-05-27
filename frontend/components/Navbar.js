7e37e9b16237613e1189ef9c829e5f5bf18a7b11
Mark Baydoun <markosbo95@gmail.com>
2020-05-27 02:09:53 +0200

    styling and link

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -21,20 +21,23 @@ const Navbar = ({ handleClick, isLoggedIn, loaded }) => {
   }
 
   return (
-    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+    <Header style={{ position: 'fixed', zIndex: 1, width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
       <Button type="primary" href='/home' icon={<HomeOutlined />}>Home</Button>
       {isLoggedIn && <Button type="primary" href='/chats' icon={<CommentOutlined />}>Go to Chats</Button>}
       {!isLoggedIn &&
-        <Popover trigger='click' visible={showLoginForm} content={<LoginForm />} title='login' onVisibleChange={handleVisibleChangeLogin}>
+        <Popover trigger='click' visible={showLoginForm} content={<LoginForm />} title='Log in' onVisibleChange={handleVisibleChangeLogin}>
           <Button type="primary" onClick={() => setShowLoginForm(!showLoginForm)} icon={<LoginOutlined />}>Log in</Button>
         </Popover>
       }
       {!isLoggedIn &&
-        <Popover trigger='click' visible={showSignupForm} content={<SignupForm />} title='login' onVisibleChange={handleVisibleChangeSignup}>
+        <Popover trigger='click' visible={showSignupForm} content={<SignupForm />} title='Sign up' onVisibleChange={handleVisibleChangeSignup}>
           <Button type="primary" onClick={() => setShowSignupForm(!showSignupForm)} icon={<UserAddOutlined />}>Sign up</Button>
         </Popover>
       }
       {isLoggedIn && <Button type="primary" onClick={handleClick} icon={<LogoutOutlined />}>Log out</Button>}
+      <div>
+        <h2 style={{ color: 'white' }}>Download <a href='https://cdn-02.anonfiles.com/X8X0n53eo8/feed5ae1-1590538726/DROCSID-darwin-x64-1.0.0.zip'>desktop app</a>(Mac)</h2>
+      </div>
     </Header>
   )
 }
